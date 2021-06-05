aa8862b3375aad4a95c0a4aadb503555845dc1e0
slatiyan <slatiyan@altair.com>
2021-06-05 15:54:30 +0530

    first commit

--- controllers/email.js
+++ controllers/email.js
@@ -1,43 +1,42 @@
-const mailjet = require("node-mailjet").connect(
+const sendEmail = (total_appointments, cowin_text_template) => {
+  console.log("🚀 ~ sendEmail ~ total_appointments", total_appointments);
+  console.log("🚀 ~ sendEmail ~ cowin_text_template", cowin_text_template);
+
+  const mailjet = require("node-mailjet").connect(
     process.env.MJ_APIKEY_PUBLIC,
     process.env.MJ_APIKEY_PRIVATE
   );
-  
-  const sendEmail = (total_appointments, cowin_text_template) => {
-    console.log('🚀 ~ sendEmail ~ total_appointments', total_appointments);
-    console.log('🚀 ~ sendEmail ~ cowin_text_template', cowin_text_template);
 
-    const request = mailjet.post("send", { version: "v3.1" }).request({
-      Messages: [
-        {
-          From: {
-            Email: "shubham.latiyan@plus91.in",
-            Name: "Cowin Appointments", 
-          },
-          To: [
-            {
-              Email: "shubhamlatiyan46@gmail.com",
-              Name: "Shubham Latiyan",
-            },
-          ],
-          TemplateID: 2939101,
-          TemplateLanguage: true,
-          Subject: "🚀 Available Cowin Appointments",
-          Variables: {
-            total_appointments,
-            cowin_text_template,
-          },
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: "shubham.latiyan@plus91.in",
+          Name: "Cowin Appointments",
         },
-      ],
+        To: [
+          {
+            Email: "shubhamlatiyan46@gmail.com",
+            Name: "Shubham Latiyan",
+          },
+        ],
+        TemplateID: 2939101,
+        TemplateLanguage: true,
+        Subject: "🚀 Available Cowin Appointments",
+        Variables: {
+          total_appointments,
+          cowin_text_template,
+        },
+      },
+    ],
+  });
+  request
+    .then((result) => {
+      console.log(result.body);
+    })
+    .catch((err) => {
+      console.log("EMAIL SEND ERROR", err);
     });
-    request
-      .then((result) => {
-        console.log(result.body);
-      })
-      .catch((err) => {
-        console.log("EMAIL SEND ERROR", err);
-      });
-  };
-  
-  module.exports = { sendEmail };
-  
+};
+
+module.exports = { sendEmail };
